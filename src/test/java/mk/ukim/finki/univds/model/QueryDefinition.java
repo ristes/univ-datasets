a66139eeacb346db86f71b22acdf1f69d10761b1
ristes <riste.stojanov@finki.ukim.mk>
2017-04-14 20:32:18 +0200

    Initial test case definition

---
+++ src/test/java/mk/ukim/finki/univds/model/QueryDefinition.java
@@ -0,0 +1,27 @@
+package mk.ukim.finki.univds.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Riste Stojanov
+ */
+@Getter
+@Setter
+public class QueryDefinition {
+
+  private String id;
+  private Long resultsNumber;
+
+  private List<Map<String, String>> params;
+
+  private String where;
+
+  private String insert;
+
+  private String select;
+
+}
