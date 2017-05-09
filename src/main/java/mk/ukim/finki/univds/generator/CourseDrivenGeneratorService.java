3f42f79b5cba0f26aed1e94a2be23db08b5904e4
ristes <riste.stojanov@finki.ukim.mk>
2017-05-09 09:53:05 +0200

    generator bug fix

--- src/main/java/mk/ukim/finki/univds/generator/CourseDrivenGeneratorService.java
+++ src/main/java/mk/ukim/finki/univds/generator/CourseDrivenGeneratorService.java
@@ -27,78 +27,82 @@ import java.util.List;
 @Service
 public class CourseDrivenGeneratorService {
 
-    private static final Logger logger = LoggerFactory.getLogger(CourseDrivenGeneratorService.class);
+  private static final Logger logger = LoggerFactory.getLogger(CourseDrivenGeneratorService.class);
 
-    @Autowired
-    private FacultyRepository facultyRepository;
+  @Autowired
+  private FacultyRepository facultyRepository;
 
-    @Autowired
-    private StaffRepositoryImpl staffRepository;
+  @Autowired
+  private StaffRepositoryImpl staffRepository;
 
-    @Autowired
-    private ControlledDatasourceGenerator datasourceGenerator;
+  @Autowired
+  private ControlledDatasourceGenerator datasourceGenerator;
 
-    @Autowired
-    private StudyprogramRepository studyProgramRepository;
+  @Autowired
+  private StudyprogramRepository studyProgramRepository;
 
-    @Autowired
-    private SubjectRepository subjectRepository;
+  @Autowired
+  private SubjectRepository subjectRepository;
 
-    @Value("${app.paths.datasets.prefix}")
-    private String datasetDirectoryPrefix;
+  @Value("${app.paths.datasets.prefix}")
+  private String datasetDirectoryPrefix;
 
-    // datasetCount == i
-    // courseCount == j
-    public void generate(int datasetCount, int courseCount) {
+  // datasetCount == i
+  // courseCount == j
+  public void generate(int datasetCount, int courseCount) {
 
-        ModelHolder.TDB_ROOT_DIR = datasetDirectoryPrefix;
-        logger.info("generating started. datasets will be stored in: {}", ModelHolder.TDB_ROOT_DIR);
-        // ============== generate base
-        // =========================================
-        // =========================================
-        // =========================================
-        Faculty faculty = FacultyFactory.make();
+    ModelHolder.TDB_ROOT_DIR = datasetDirectoryPrefix;
+    logger.info("generating started. datasets will be stored in: {}", ModelHolder.TDB_ROOT_DIR);
+    // ============== generate base
+    // =========================================
+    // =========================================
+    // =========================================
+    Faculty faculty = FacultyFactory.make();
 
-        User superAdministrator = UserFactory.make(UserFactory.SUPER_ADMINISTRATOR_TYPE);
-        superAdministrator.setFaculty(faculty);
+    User superAdministrator = UserFactory.make(UserFactory.SUPER_ADMINISTRATOR_TYPE);
+    superAdministrator.setFaculty(faculty);
 
-        List<User> technicalStaff = UserFactory.make(3, UserFactory.TECHNICAL_STAFF_TYPE);
-        technicalStaff.forEach(technicalStaffPerson -> technicalStaffPerson.setFaculty(faculty));
+    List<User> technicalStaff = UserFactory.make(3, UserFactory.TECHNICAL_STAFF_TYPE);
+    technicalStaff.forEach(technicalStaffPerson -> technicalStaffPerson.setFaculty(faculty));
 
-        // each application start should have its own study program where all relations will be added
-        StudyProgram studyProgram = StudyprogramFactory.make();
-        studyProgram.setFaculty(faculty);
-        // =========================================
-        // =========================================
-        // =========================================
-        for (int j = 0; j < datasetCount; j++) {
-            for (int i = 1; i < courseCount; i++) {
-                String datasetName = j + "" + i;
-                logger.info("now generating {}", datasetName);
-                ModelHolder.createInitialDataset(datasetName);
+    // each application start should have its own study program where all relations will be added
+    StudyProgram studyProgram = StudyprogramFactory.make();
+    studyProgram.setFaculty(faculty);
+    // =========================================
+    // =========================================
+    // =========================================
+    for (int j = 0; j < datasetCount; j++) {
+      for (int i = 1; i < courseCount; i++) {
+        String datasetName = j + "" + i;
+        logger.info("now generating {}", datasetName);
+        ModelHolder.createInitialDataset(datasetName);
 
-                // save the base so its the same for all ds. This can be done better...
-                facultyRepository.save(faculty);
-                staffRepository.save(superAdministrator);
-                staffRepository.save(technicalStaff);
-                studyProgramRepository.save(studyProgram);
-                // base saved.
+        // save the base so its the same for all ds. This can be done better...
+        facultyRepository.save(faculty);
+        staffRepository.save(superAdministrator);
+        staffRepository.save(technicalStaff);
+        studyProgramRepository.save(studyProgram);
+        // base saved.
 
-                // generate professors
-                User professor = UserFactory.make(UserFactory.PROFESSOR_TYPE);
-                professor.setFaculty(studyProgram.getFaculty());
-                staffRepository.save(professor);
+        // generate professors
+        User professor = UserFactory.make(UserFactory.PROFESSOR_TYPE);
+        professor.setFaculty(studyProgram.getFaculty());
+        staffRepository.save(professor);
 
-                // generate subjects
-                Subject subject = SubjectFactory.make();
-                subject.getStudyPrograms().add(studyProgram);
-                subjectRepository.save(subject);
+        // generate subjects
+        Subject subject = SubjectFactory.make();
+        subject.getStudyPrograms().add(studyProgram);
+        subjectRepository.save(subject);
 
-                double numberOfGrades = i * 100 * Math.pow(10, j);
-                Long courseId = (long) 10 * j + i;
-                datasourceGenerator.generateGradeCourseRelation(professor, subject, numberOfGrades, courseId);
-                ModelHolder.closeDataset();
-            }
+        for (int k = 0; k <= j; k++) {
+          for (int t = 1; t < courseCount; t++) {
+            double numberOfGrades = t * 100 * Math.pow(10, k);
+            Long courseId = (long) 10 * k + t;
+            datasourceGenerator.generateGradeCourseRelation(professor, subject, numberOfGrades, courseId);
+          }
         }
+        ModelHolder.closeDataset();
+      }
     }
+  }
 }
